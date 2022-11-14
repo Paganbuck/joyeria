e61701c9e7da5a88b8abc2435e6d687353b79d8d
Juan Diego Bedoya <jdbedoya928@gmail.com>
2022-11-13 22:49:42 -0500

    Conectamos a DB Mongoose

--- back/server.js
+++ back/server.js
@@ -2,7 +2,11 @@ const app=require("./app")
 
 //setear el archivo de configuración
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/database");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configuramos a base de datos
+connectDatabase();
 
 //Llamemos el servidor
 const server=app.listen(process.env.PORT, () => {
